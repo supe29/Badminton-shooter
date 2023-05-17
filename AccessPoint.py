d2cf996c93c86de9f4cead54808cdbb3ae55da6b
Tackounet <Tackounet>
2023-05-17 19:25:10 +0200

    Push Credentials in another file

--- AccessPoint.py
+++ AccessPoint.py
@@ -1,30 +1,36 @@
 import network
 import socket
+import Credentials
 from utime import sleep
 
+
+
 class AccessPoint:
-  ssid = 'BadmintonShooter'
-  password = '1234567890'
-  sock = socket.socket()
 
   @classmethod
   def init(self):
-    access_point = network.WLAN(network.AP_IF)
-    access_point.config(essid=self.ssid, password=self.password)
-    access_point.active(True)
+    self._access_point = network.WLAN(network.AP_IF)
+    self._access_point.config(essid=Credentials.ssid, password=Credentials.password)
+    self._access_point.active(True)
 
     # Wait for wifi
-    while access_point.active() == False:
+    while self._access_point.active() == False:
       sleep(0.5)
 
     print('WiFi active')
-    status = access_point.ifconfig()
+    status = self._access_point.ifconfig()
 
     # Open socket
     # Default address => 192.168.4.1
     ip = status[0]
     addr = (ip, 80)
+    self.sock = socket.socket()
     self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     self.sock.bind(addr)
     self.sock.listen(1)
     print('listening on', addr)
+
+
+  @classmethod
+  def close(self):
+    self._access_point.active(False)
---
+++ Credentials.py
@@ -0,0 +1,3 @@
+class Credentials:
+  ssid = 'BadmintonShooter'
+  password = '1234567890'
